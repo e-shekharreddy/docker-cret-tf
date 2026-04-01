91c0c711253acc7f6d7b39c2000698731fc31972
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-04-01 11:49:28 +0530

    terraform

---
+++ bastion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# we are creating 50Gb root disk, but only 20gb is partioned 
+# reamaing 30GB we need to exted using below commands
+growpart /dev/nvme0n1 4
+lvextend -r -L +30G /dev/RootVG/varVol
+sudo xfs_growfs /var
+
+
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl enable docker
+systemctl start docker
+usermod -aG docker ec2-user
---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "docker" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  user_data = file("bastion.sh")
+
+  tags = {
+    Name = "docker"
+    Project = "docker-roboshop"
+
+  }
+}
+
